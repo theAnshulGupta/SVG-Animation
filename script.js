8bd367b5c59a8aefe9f044fecbcf6285c41cc3a6
Anshul Gupta <hi.anshulg@gmail.com>
2020-11-25 22:44:57 -0800

    intial commit

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const text = document.querySelectorAll("#anshul path");
 console.log(text);
 for (let i = 0; i < text.length; i++) {
-  console.log(text[i].getTotalLength());
+  var n = text[i].getTotalLength();
+  console.log(n);
 }
